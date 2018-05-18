302f4293fc128807e8a758007c9b6f78df6f8c6c
Ben Kim <bhk1019@gmail.com>
2018-05-18 01:16:52 +0000

    completed and tested tree traversals

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,13 +1,38 @@
 class Tree
   attr_accessor :payload, :children
 
-  def initialize(payload, children)
+  def initialize(payload, children=[])
     @payload = payload
     @children = children
   end
 
   def depth_first(data)
-    
+    # Base Case - Node Match
+    @children.each do |child|
+      if child.payload == data
+        return child
+      elsif child.children != []
+        return child.depth_first(data)
+      end
+    end
+    if self.payload == data
+      return self
+    end
+  end
+
+  def breadth_first(data)
+    queue = [self]
+    queue.each do |item|
+      if item.payload == data
+        return item
+      else
+        item.children.each do |child|
+          queue.push(child)
+        end
+        queue.shift
+      end
+    end
   end
 
 end
+
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Tree, type: :model do
-  describe "depth_first should find the right node" do
+  describe "depth_first tree traversal method" do
+
+    it "should return left given a simple tree with one trunk and two leaves" do
+      left = Tree.new(15, [])
+      right = Tree.new(12, [])
+      trunk = Tree.new(2, [left, right])
+      expect(trunk.depth_first(15)).to eq left
+    end
+
     it "should return the correct node given a tree with a node of 11" do
       deep_fifth_node = Tree.new(5, [])
       eleventh_node = Tree.new(11, [])
@@ -14,5 +22,28 @@ RSpec.describe Tree, type: :model do
       expect(trunk.depth_first(11)).to eq eleventh_node
     end
 
+  end
+
+  describe "breadth_first tree traversal method" do
+
+    it "should return left given a simple tree with one trunk and two leaves" do
+      left = Tree.new(15, [])
+      right = Tree.new(12, [])
+      trunk = Tree.new(2, [left, right])
+      expect(trunk.depth_first(15)).to eq left
+    end
+
+    it "should return the correct node given a tree with a node of 11" do
+      deep_fifth_node = Tree.new(5, [])
+      eleventh_node = Tree.new(11, [])
+      fourth_node   = Tree.new(4, [])
+      ninth_node = Tree.new(9, [fourth_node])
+      sixth_node = Tree.new(6, [deep_fifth_node, eleventh_node])
+      seventh_node = Tree.new(7, [sixth_node])
+      shallow_fifth_node = Tree.new(5, [ninth_node])
+      trunk   = Tree.new(2, [seventh_node, shallow_fifth_node])
+      expect(trunk.depth_first(11)).to eq eleventh_node
+    end
+    
   end
 end
